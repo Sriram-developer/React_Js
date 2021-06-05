3666d84ccbff1072380d17ffc144b8c24bcdcdd8
M.SRIRAM <82840132+Sriram-developer@users.noreply.github.com>
2021-06-05 16:53:02 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+function App(){
+    return (<div>
+        <Header />
+        <Footer />
+        <Note />
+        <Note />
+        <Note />
+    </div>);
+}
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Note(){
+    return(
+       <div className="note"><h1>Javacript and React.Js</h1>
+         
+         <p>This was an amazing bootcamp taken by sriram.
+             we covered everything from scraches including Javascript and React.Js, Html and Css</p> 
+       </div>
+    );
+}
+export default Note;
